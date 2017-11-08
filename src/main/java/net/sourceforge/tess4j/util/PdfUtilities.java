ff6a726c0866faaf4cf945774f45527431b05f1e
nguyenq <nguyenq@users.sf.net>
2017-11-08 11:56:25 -0600

    Use default temp directory for working images

--- src/main/java/net/sourceforge/tess4j/util/PdfUtilities.java
+++ src/main/java/net/sourceforge/tess4j/util/PdfUtilities.java
@@ -19,6 +19,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -56,18 +58,15 @@ public class PdfUtilities {
         } catch (NoClassDefFoundError ncdfe) {
             throw new RuntimeException(getMessage(ncdfe.getMessage()));
         } finally {
-            if (pngFiles != null && pngFiles.length>0) {
-	            //Get the working directory of the PNG files.
+            if (pngFiles != null && pngFiles.length > 0) {
+                // get the working directory of the PNG files
                 File pngDirectory = new File(pngFiles[0].getParent());
                 // delete temporary PNG images
                 for (File tempFile : pngFiles) {
                     tempFile.delete();
                 }
-                //Delete the working directory only if it was created with having same name as the pdf file.
-                if(pngDirectory.getAbsoluteFile().getName().equals(inputPdfFile.getName().replace(".pdf", "")))
-                {
-                    pngDirectory.delete();
-                }
+
+                pngDirectory.delete();
             }
         }
     }
@@ -77,16 +76,11 @@ public class PdfUtilities {
      *
      * @param inputPdfFile input file
      * @return an array of PNG images
+     * @throws java.io.IOException
      */
-    @SuppressWarnings("unused")
-    public synchronized static File[] convertPdf2Png(File inputPdfFile) {
-        File imageDir = new File(inputPdfFile.getParentFile()+System.getProperty("file.separator")+inputPdfFile.getName().replace(".pdf", ""));
-        imageDir.mkdirs();
-
-        if (imageDir == null) {
-            String userDir = System.getProperty("user.dir");
-            imageDir = new File(userDir);
-        }
+    public synchronized static File[] convertPdf2Png(File inputPdfFile) throws IOException {
+        Path path = Files.createTempDirectory("tessimages");
+        File imageDir = path.toFile();
 
         //get Ghostscript instance
         Ghostscript gs = Ghostscript.getInstance();
