d5890161f19157e03069cdffff94b751154d378d
nguyenq <nguyenq@users.sf.net>
2018-03-20 22:16:50 -0500

    Utilize both GS and PDFBox support with GS as default

--- src/main/java/net/sourceforge/tess4j/util/PdfUtilities.java
+++ src/main/java/net/sourceforge/tess4j/util/PdfUtilities.java
@@ -15,28 +15,19 @@
  */
 package net.sourceforge.tess4j.util;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
-import org.apache.pdfbox.io.MemoryUsageSetting;
-import org.apache.pdfbox.multipdf.PDFMergerUtility;
-import org.apache.pdfbox.multipdf.Splitter;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.ImageType;
-import org.apache.pdfbox.rendering.PDFRenderer;
-import org.apache.pdfbox.tools.imageio.ImageIOUtil;
-import org.slf4j.LoggerFactory;
-
+/**
+ * PDF utilities based on Ghostscript or PDFBox with Ghostscript as default. If
+ * Ghostscript is not available on the system, then PDFBox is used. Call
+ * <code>System.setProperty(PDF_LIBRARY, PDFBOX);</code> to set PDFBox as
+ * default.
+ */
 public class PdfUtilities {
 
-    public static final String GS_INSTALL = "\nPlease download, install GPL Ghostscript from http://www.ghostscript.com\nand/or set the appropriate environment variable.";
-
-    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(new LoggHelper().toString());
+    public static final String PDF_LIBRARY = "pdf.library";
+    public static final String PDFBOX = "pdfbox";
 
     /**
      * Converts PDF to TIFF format.
@@ -46,25 +37,14 @@ public class PdfUtilities {
      * @throws IOException
      */
     public static File convertPdf2Tiff(File inputPdfFile) throws IOException {
-        File[] pngFiles = null;
-
-        try {
-            pngFiles = convertPdf2Png(inputPdfFile);
-            File tiffFile = File.createTempFile("multipage", ".tif");
-
-            // put PNG images into a single multi-page TIFF image for return
-            ImageIOHelper.mergeTiff(pngFiles, tiffFile);
-            return tiffFile;
-        } catch (UnsatisfiedLinkError ule) {
-            throw new RuntimeException(getMessage(ule.getMessage()));
-        } catch (NoClassDefFoundError ncdfe) {
-            throw new RuntimeException(getMessage(ncdfe.getMessage()));
-        } finally {
-            if (pngFiles != null) {
-                // delete temporary PNG images
-                for (File tempFile : pngFiles) {
-                    tempFile.delete();
-                }
+        if (PDFBOX.equals(System.getProperty(PDF_LIBRARY))) {
+            return PdfBoxUtilities.convertPdf2Tiff(inputPdfFile);
+        } else {
+            try {
+                return PdfGsUtilities.convertPdf2Tiff(inputPdfFile);
+            } catch (Exception e) {
+                System.setProperty(PDF_LIBRARY, PDFBOX);
+                return convertPdf2Tiff(inputPdfFile);
             }
         }
     }
@@ -74,56 +54,19 @@ public class PdfUtilities {
      *
      * @param inputPdfFile input file
      * @return an array of PNG images
+     * @throws java.io.IOException
      */
-    public static File[] convertPdf2Png(File inputPdfFile)  {
-        File imageDir = inputPdfFile.getParentFile();
-
-        if (imageDir == null) {
-            String userDir = System.getProperty("user.dir");
-            imageDir = new File(userDir);
-        }
-
-        PDDocument document = null;
-        try {
-            document = PDDocument.load(inputPdfFile);
-            PDFRenderer pdfRenderer = new PDFRenderer(document);
-            for (int page = 0; page < document.getNumberOfPages(); ++page) {
-                BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
-
-                // suffix in filename will be used as the file format
-                String filename = String.format("workingimage%03d.png", page + 1);
-                ImageIOUtil.writeImage(bim, new File(imageDir, filename).getAbsolutePath(), 300);
+    public static File[] convertPdf2Png(File inputPdfFile) throws IOException {
+        if (PDFBOX.equals(System.getProperty(PDF_LIBRARY))) {
+            return PdfBoxUtilities.convertPdf2Png(inputPdfFile);
+        } else {
+            try {
+                return PdfGsUtilities.convertPdf2Png(inputPdfFile);
+            } catch (Exception e) {
+                System.setProperty(PDF_LIBRARY, PDFBOX);
+                return convertPdf2Png(inputPdfFile);
             }
         }
-        catch (IOException ioe) {
-            logger.error("Error extracting PDF Document => " + ioe);
-        }
-        finally {
-            if (document != null) {
-                try {
-                    document.close();
-                }
-                catch (Exception e) {}
-            }
-        }
-
-        // find working files
-        File[] workingFiles = imageDir.listFiles(new FilenameFilter() {
-
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.toLowerCase().matches("workingimage\\d{3}\\.png$");
-            }
-        });
-
-        Arrays.sort(workingFiles, new Comparator<File>() {
-            @Override
-            public int compare(File f1, File f2) {
-                return f1.getName().compareTo(f2.getName());
-            }
-        });
-
-        return workingFiles;
     }
 
     /**
@@ -156,48 +99,15 @@ public class PdfUtilities {
      * @param lastPage end page
      */
     public static void splitPdf(File inputPdfFile, File outputPdfFile, int firstPage, int lastPage) {
-        PDDocument document = null;
-        try {
-            document = PDDocument.load(inputPdfFile);
-            Splitter splitter = new Splitter();
-
-            splitter.setStartPage(firstPage);
-            splitter.setEndPage(lastPage);
-            splitter.setSplitAtPage(lastPage-firstPage);
-
-            List<PDDocument> documents = splitter.split(document);
-
-            if (documents.size() == 1) {
-                PDDocument outputPdf = documents.get(0);
-                outputPdf.save(outputPdfFile);
-                outputPdf.close();
-            }
-            else {
-                logger.error("Splitter returned " + documents.size() + " documents rather than expected of 1");
-            }
-        }
-        catch (IOException ioe) {
-            logger.error("Exception splitting PDF => " + ioe);
-        }
-        finally {
-            if (document != null) {
-                try {
-                    document.close();
-                }
-                catch (Exception e) {}
-            }
-        }
-    }
-
-    private static final String PS_FILE = "lib/pdfpagecount.ps";
-    private static final String pdfPageCountFilePath;
-
-    static {
-        File postscriptFile = LoadLibs.extractTessResources(PS_FILE);
-        if (postscriptFile != null && postscriptFile.exists()) {
-            pdfPageCountFilePath = postscriptFile.getPath();
+        if (PDFBOX.equals(System.getProperty(PDF_LIBRARY))) {
+            PdfBoxUtilities.splitPdf(inputPdfFile, outputPdfFile, firstPage, lastPage);
         } else {
-            pdfPageCountFilePath = PS_FILE;
+            try {
+                PdfGsUtilities.splitPdf(inputPdfFile, outputPdfFile, firstPage, lastPage);
+            } catch (Exception e) {
+                System.setProperty(PDF_LIBRARY, PDFBOX);
+                splitPdf(inputPdfFile, outputPdfFile, firstPage, lastPage);
+            }
         }
     }
 
@@ -220,21 +130,14 @@ public class PdfUtilities {
      * @return number of pages
      */
     public static int getPdfPageCount(File inputPdfFile) {
-        PDDocument document = null;
-        try {
-            document = PDDocument.load(inputPdfFile);
-            return document.getNumberOfPages();
-        }
-        catch (IOException ioe) {
-            logger.error("Error counting PDF pages => " + ioe);
-            return - 1;
-        }
-        finally {
-            if (document != null) {
-                try {
-                    document.close();
-                }
-                catch (Exception e) {}
+        if (PDFBOX.equals(System.getProperty(PDF_LIBRARY))) {
+            return PdfBoxUtilities.getPdfPageCount(inputPdfFile);
+        } else {
+            try {
+                return PdfGsUtilities.getPdfPageCount(inputPdfFile);
+            } catch (Exception e) {
+                System.setProperty(PDF_LIBRARY, PDFBOX);
+                return getPdfPageCount(inputPdfFile);
             }
         }
     }
@@ -246,23 +149,15 @@ public class PdfUtilities {
      * @param outputPdfFile output file
      */
     public static void mergePdf(File[] inputPdfFiles, File outputPdfFile) {
-        try {
-            PDFMergerUtility mergerUtility = new PDFMergerUtility();
-            mergerUtility.setDestinationFileName(outputPdfFile.getPath());
-            for (File inputPdfFile : inputPdfFiles) {
-                mergerUtility.addSource(inputPdfFile);
+        if (PDFBOX.equals(System.getProperty(PDF_LIBRARY))) {
+            PdfBoxUtilities.mergePdf(inputPdfFiles, outputPdfFile);
+        } else {
+            try {
+                PdfGsUtilities.mergePdf(inputPdfFiles, outputPdfFile);
+            } catch (Exception e) {
+                System.setProperty(PDF_LIBRARY, PDFBOX);
+                mergePdf(inputPdfFiles, outputPdfFile);
             }
-            mergerUtility.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
         }
-        catch (IOException ioe) {
-            logger.error("Error counting PDF pages => " + ioe);
-        }
-    }
-
-    static String getMessage(String message) {
-        if (message.contains("library 'gs") || message.contains("ghost4j")) {
-            return message + GS_INSTALL;
-        }
-        return message;
     }
 }
