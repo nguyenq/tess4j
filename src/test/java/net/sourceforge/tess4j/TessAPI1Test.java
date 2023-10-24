ecc54dfefaf83e53fb466a87299660d7b573ae58
nguyenq <nguyenq@users.sf.net>
2023-10-24 09:53:47 -0500

    Upgrade to Tesseract 5.3.3

--- src/test/java/net/sourceforge/tess4j/TessAPI1Test.java
+++ src/test/java/net/sourceforge/tess4j/TessAPI1Test.java
@@ -198,7 +198,7 @@ public class TessAPI1Test {
     @Test
     public void testTessVersion() {
         logger.info("TessVersion");
-        String expResult = "5.3.2";
+        String expResult = "5.3.3";
         String result = TessAPI1.TessVersion();
         logger.info(result);
         assertTrue(result.startsWith(expResult));
